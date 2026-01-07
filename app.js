8ad921be585eefdb1857153f1829b55890062a30
Ayesh Alexan Anthonythasan <ayeshanthoythasan@gmail.com>
2026-01-07 11:27:21 +0530

    Patch

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 import { registerRootComponent } from 'expo';
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 
 function App() {
-  return <AppNavigator />;
+  return (
+    <View style={styles.container}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+  },
+});
 
 registerRootComponent(App);
